f47a08ff764ccfede3f05e89400d3a03e8177bbe
Opilane TTHK <opilane@tthk.ee>
2021-10-19 10:19:25 +0300

    add button and gallery array

---
+++ app.js
@@ -0,0 +1,15 @@
+const currentImage = document.querySelector('#image');
+const images = ['images/pilt1.jpg', 'images/pilt2.webp', 'images/pilt3.webp' ];
+
+
+function changeImage() {
+    console.log('Hello from script file.');
+
+    let randomNumber = Math.floor(Math.random() * images.length);
+
+    console.log(images[randomNumber]);
+
+    currentImage.src = images[randomNumber];
+}
+
+
